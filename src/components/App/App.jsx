01dd35702f7f642c2d3bf1343e7e177e89ca2763
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2024-10-17 13:18:14 -0300

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+
+import Header from '../Header/Header'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Hola</h1>
+      <Header />
+       
+    </>
+  )
+}
+
+export default App
