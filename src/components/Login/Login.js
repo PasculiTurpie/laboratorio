4d5149328ebba3cf98884bef1a9d5581433835fa
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2024-10-19 14:34:43 -0300

    feat:Implementacion de inputs tipo google

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -8,11 +8,31 @@ const Login = () => {
   const nameLabel = useRef(null);
   const passInput = useRef(null);
   const passLabel = useRef(null);
+  const input = useRef('')
 
+
+
+  const listInputs = Array.from(document.querySelectorAll(".form-input"));
+  console.log(listInputs)
+
+  const lists = listInputs.map((input) => {
+    const id = input.id;
+    console.log(`error-input-${id}`)
+  })
+
+  const handleSubmit = (e) => {
+   e.preventDefault()
+ }
+
+ /*  const handleOnChange = (e) => {
+    console.log(e.target.validity.valid);
+    console.log(`error-input-${id}`);
+  } */
 
   const handleInputNameFocus = () => {
     const nameInputRef = nameInput.current;
     const nameLabelRef = nameLabel.current;
+    console.log(nameInputRef.validity.valid);
     nameInputRef.classList.add("focus");
     nameLabelRef.classList.add("focus");
     nameLabelRef.classList.add("top");
@@ -21,26 +41,31 @@ const Login = () => {
   const handleInputNameBlur = () => {
     const nameInputRef = nameInput.current;
     const nameLabelRef = nameLabel.current;
-    nameInputRef.classList.remove("focus");
-    nameLabelRef.classList.remove("focus");
-    nameLabelRef.classList.remove("top");
+    if (nameInputRef.value === "") {
+      nameInputRef.classList.remove("focus");
+      nameLabelRef.classList.remove("focus");
+      nameLabelRef.classList.remove("top");
+    }
   };
 
-const handleInputPassFocus = () => {
-  const passInputRef = passInput.current;
-  const passLabelRef = passLabel.current;
-  passInputRef.classList.add("focus");
-  passLabelRef.classList.add("focus");
-  passLabelRef.classList.add("top");
-};
+  const handleInputPassFocus = () => {
+    const passInputRef = passInput.current;
+    const passLabelRef = passLabel.current;
+    console.log(passInputRef.validity.valid);
+    passInputRef.classList.add("focus");
+    passLabelRef.classList.add("focus");
+    passLabelRef.classList.add("top");
+  };
 
-const handleInputPassBlur = () => {
-  const passInputRef = passInput.current;
-  const passLabelRef = passLabel.current;
-  passInputRef.classList.remove("focus");
-  passLabelRef.classList.remove("focus");
-  passLabelRef.classList.remove("top");
-};
+  const handleInputPassBlur = () => {
+    const passInputRef = passInput.current;
+    const passLabelRef = passLabel.current;
+    if (passInputRef.value === "") {
+      passInputRef.classList.remove("focus");
+      passLabelRef.classList.remove("focus");
+      passLabelRef.classList.remove("top");
+    }
+  };
 
   return (
     <div className="login">
@@ -49,7 +74,7 @@ const handleInputPassBlur = () => {
         src="http://principedeasturiasvaldivia.cl/Colegio/wp-content/uploads/2020/04/cropped-logo_colegio_completo-2.png"
         alt=""
       />
-      <form action="" className="form-login">
+      <form action="" className="form-login" onSubmit={handleSubmit}>
         <div className="form-group-input">
           <span className="form-label" ref={nameLabel}>
             Nombre
@@ -62,6 +87,11 @@ const handleInputPassBlur = () => {
             ref={nameInput}
             onFocus={handleInputNameFocus}
             onBlur={handleInputNameBlur}
+            required
+            autoComplete="off"
+            minLength={3}
+            maxLength={30}
+            /* onChange={handleOnChange} */
           />
           <span className="error-input error-input-name">Mensaje</span>
         </div>
@@ -77,6 +107,8 @@ const handleInputPassBlur = () => {
             ref={passInput}
             onFocus={handleInputPassFocus}
             onBlur={handleInputPassBlur}
+            required
+            minLength={8}
           />
           <span className="error-input error-input-password">Mensaje</span>
         </div>
